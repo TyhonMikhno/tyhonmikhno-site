d6d196d141a0ad30a157fb569003f33a060f6f54
Tyhon Mikhno <timeair10000@gmail.com>
2024-05-15 23:56:26 +0300

    Commit changes.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Tyhon`s site <3',
+  title: 'Tyhon`s site <333',
   tagline: 'КП-33(i love capybaras)',
   favicon: 'img/favicon.ico',
 
